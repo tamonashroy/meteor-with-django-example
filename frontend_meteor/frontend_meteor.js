93408bfd28381ee90a87bc9a994999c1e16ed0c2
Tamonash Roy <tamonashroy@gmail.com>
2015-10-06 02:31:11 +0530

    Add Packages and initial code

--- frontend_meteor/frontend_meteor.js
+++ frontend_meteor/frontend_meteor.js
@@ -1,23 +1,11 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+FlowRouter.route('/', {
+          action: function() {
+              BlazeLayout.render("mainLayout", {content: "blogHome"});
+                }
+                });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+FlowRouter.route('/:postId', {
+          action: function() {
+              BlazeLayout.render("mainLayout", {content: "blogPost"});
+                }
+                });
