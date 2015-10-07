d579f22e090ba16203f73e474dd0b55f9443b2c2
Tamonash Roy <tamonashroy@gmail.com>
2015-10-08 03:33:32 +0530

    Build rest connection between client and server

--- frontend_meteor/frontend_meteor.js
+++ frontend_meteor/frontend_meteor.js
@@ -20,10 +20,21 @@ if (Meteor.isClient) {
    });
    Template.WhatToReadNext.helpers({
 	articles: function() {
-		var data = Article.find({});
-		return data;
+		var data = Article.find().fetch();
+		var pn = Article.findOne({'doc': 1}).pageno;
+		var items = data.slice(pn*4, pn*4+4)
+		return items;
 	}
    });
+   /*Template.WhatToReadNext.onRendered(function() {
+	// This is run once and set the page no to 0. This value is used to determine 
+	// which block of 4 set to show in the what to read next section.
+	//Article.update({'doc':1}, {$set:{'pageno':0}});
+	console.log(Article.findOne({doc:1}));
+	var a=Article.findOne({doc:1});
+	Session.set('page', a._id);
+	console.log("set pageno session"+session.get('page'));
+   });*/
    Template.ArticleList.helpers({
 	article: function() {
 		var data = Article.findOne({articleid: 1});
@@ -34,4 +45,70 @@ if (Meteor.isClient) {
 		return data;
 	}  
    });
+   Template.WhatToReadNext.events({
+	   'click #next': function(event){
+		   console.log("Click event in next happened");
+		   var pageInfoDoc = Article.findOne({'doc':1});
+		   Session.set('page', pageInfoDoc._id)
+		   var pageNo = pageInfoDoc.pageno;
+		   console.log("Page number now is " + pageNo);
+		   var noRec = Article.find().count();
+		   console.log("No of recs now is " + noRec);
+		   var maxPageNo = Math.ceil(noRec / 4);
+		   console.log("Max Page No " + maxPageNo);
+		   if (pageNo < maxPageNo-1) {
+			console.log("no errors here");
+			Article.update(Session.get('page'), {$inc: {pageno:1}});
+		}
+	   },
+	   'click #prev': function(event){
+		   console.log("Click event in next happened");
+		   var pageInfoDoc = Article.findOne({'doc':1});
+		   Session.set('page', pageInfoDoc._id)
+		   var pageNo = pageInfoDoc.pageno;
+		   console.log("Page number now is " + pageNo);
+		   if (pageNo > 0) {
+			Article.update(Session.get('page'), {$inc: {pageno:-1}});
+		}
+	   }
+   });
+   
 }
+if (Meteor.is_server) {
+	Meteor.startup(function() {
+	Meteor.methods({
+		'logme': function() {
+			console.log("Logme cakked");
+		},
+		'fetchFromService': function() {
+			var url = "http://tamonashroy.pythonanywhere.com/articles";
+			//synchronous GET
+			var result = Meteor.http.get(url, {timeout:30000});
+			if(result.statusCode==200) {
+				var respJson = JSON.parse(result.content);
+				console.log("response received.");
+				for (i=0; i<respJson.length; i++){
+					var temp = respJson[i].id;
+					if (!Article.findOne({'id': temp})) {
+						Article.insert(respJson[i]);
+					}
+
+				}
+				return respJson;
+			} else {
+				console.log("Response issue: ", result.statusCode);
+				var errorJson = JSON.parse(result.content);
+				throw new Meteor.Error(result.statusCode, errorJson.error);
+			}
+		},
+		'startPolling': function(seconds){
+        		pollingTaskID = Meteor.setInterval(function(){
+            		Meteor.call('fetchFromService');
+        		}, seconds*1000);
+        		return "polling started...";
+    		}
+		
+	});
+});
+}
+
