69255bd81c5b5b6b3921a11354f9f760eb479c15
Tamonash Roy <tamonashroy@gmail.com>
2015-10-07 14:12:55 +0530

    Add Detail and ListPages

--- frontend_meteor/frontend_meteor.js
+++ frontend_meteor/frontend_meteor.js
@@ -1,11 +1,37 @@
 FlowRouter.route('/', {
-          action: function() {
-              BlazeLayout.render("mainLayout", {content: "blogHome"});
-                }
-                });
+   action: function() {
+   BlazeLayout.render("mainLayout", {content: "ArticleList"});
+  }
+});
 
-FlowRouter.route('/:postId', {
-          action: function() {
-              BlazeLayout.render("mainLayout", {content: "blogPost"});
-                }
-                });
+FlowRouter.route('/article/:articleid', {
+      action: function() {
+      BlazeLayout.render("mainLayout", {content: "DetailPage"});
+   }
+});
+Article = new Mongo.Collection("articles");
+if (Meteor.isClient) {
+   Template.DetailPage.helpers({
+	article: function() {
+		var articleid = FlowRouter.getParam("articleid");
+		var data = Article.findOne({articleid: parseInt(articleid)});
+		return data;
+	}
+   });
+   Template.WhatToReadNext.helpers({
+	articles: function() {
+		var data = Article.find({});
+		return data;
+	}
+   });
+   Template.ArticleList.helpers({
+	article: function() {
+		var data = Article.findOne({articleid: 1});
+		return data;
+	},
+	articles: function() {
+		var data = Article.find({});
+		return data;
+	}  
+   });
+}
